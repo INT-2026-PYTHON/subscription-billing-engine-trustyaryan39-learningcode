70cccfdf30fb7b6805905171c4bbc9d66f04de5f
trustyaryan39-learningcode <aryan.code39@gmail.com>
2026-06-15 16:32:06 +0530

    Add Discount

--- billing_engine_starter/billing_engine/discounts/fixed.py
+++ billing_engine_starter/billing_engine/discounts/fixed.py
@@ -11,9 +11,11 @@ from billing_engine.discounts.base import Discount, DiscountContext
 
 class FixedAmountDiscount(Discount):
     def __init__(self, amount: Money) -> None:
-        # TODO Day 1
-        raise NotImplementedError("Day 1: implement FixedAmountDiscount.__init__")
+        if amount.is_negative():
+            raise ValueError("Discount amount must be non-negative")
+        self.amount = amount
 
     def apply(self, subtotal: Money, context: DiscountContext) -> Money:
-        # TODO Day 1
-        raise NotImplementedError("Day 1: implement FixedAmountDiscount.apply")
+        if self.amount.currency != subtotal.currency:
+            raise ValueError("Discount amount and subtotal must have the same currency")
+        return min(self.amount, subtotal)
